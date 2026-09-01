dec061a018bb2753790539124170e3b22442e57f
DS-ann <dsannigrahi5956@gmail.com>
2026-09-01 15:51:53 +0530

    Fix HTML doctype declaration in worker.js

--- cloudflare/src/worker.js
+++ cloudflare/src/worker.js
@@ -7,7 +7,7 @@ const CORS = {
 const MAX_ATTACHMENT_BYTES = 1400000;
 
 const HTML = `
-<!doctype html>
+<!doctype html
 <html>
 <head>
 <meta name="viewport" content="width=device-width,initial-scale=1">
